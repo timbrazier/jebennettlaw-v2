b5d23b1909c68e262b344840dcd1e60fc84c6713
Tim Brazier <me@timbrazier.co.uk>
2015-10-22 00:30:50 +0100

    Adds the changes requested by Mike

---
+++ _site/scripts/show-hide.js
@@ -0,0 +1,15 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+
+$(document).ready(function(){
+
+$(".slidingDiv").hide();
+$(".show_hide").show();
+
+$('.show_hide').click(function(){
+$(".slidingDiv").slideToggle();
+});
+
+});
+
+</script>
--- contact.md
+++
@@ -1,7 +0,0 @@
----
-layout: page
-title: Contact us
-permalink: /contact-us/
----
-
-{% include warning.html %}
